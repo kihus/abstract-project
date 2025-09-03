13be031da0a4c3baa4c9bd0d4345bb39d0467696
Bruno Nascimento <wintergamesk@gmail.com>
2025-09-03 16:59:40 -0300

    feat: Add list person, taxes paid and total taxes

--- AbstractProject/Program.cs
+++ AbstractProject/Program.cs
@@ -1,6 +1,8 @@
 ﻿using AbstractProject.Entities;
 
-Console.WriteLine("Enter the number of tax payers: ");
+var person = new List<Person>();
+
+Console.Write("Enter the number of tax payers: ");
 var numberPayers = int.Parse(Console.ReadLine() ?? "0");
 
 for (int i = 1; i <= numberPayers; i++)
@@ -15,4 +17,33 @@ for (int i = 1; i <= numberPayers; i++)
 
 	Console.Write("Annual Income: ");
 	var annualIncome = double.Parse(Console.ReadLine() ?? "0");
+
+	if(typePerson.ToString().ToLower() == "i")
+	{
+		Console.Write("Health expenses: ");
+		var healthExpenses = double.Parse(Console.ReadLine() ?? "0");
+
+		person.Add(new NaturalPerson(name, annualIncome, healthExpenses));
+	}
+	else
+	{
+		Console.Write("Number of employees: ");
+		var employees = int.Parse(Console.ReadLine() ?? "0");
+
+		person.Add(new LegalEntity(name, annualIncome, employees));
+	}
 }
+
+Console.WriteLine();
+Console.WriteLine("TAXES PAID: ");
+var totalTaxes = 0.0;
+foreach(var item in person)
+{
+	Console.WriteLine($"{item.Name}: $ {item.PersonTax():F2}");
+	totalTaxes += (double)item.AnnualIncome;
+}
+
+Console.WriteLine();
+Console.WriteLine($"TOTAL TAXES: ${totalTaxes:F2}");
+
+
