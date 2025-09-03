188ac4743299544f23d28354834b55459316bddf
Bruno Nascimento <wintergamesk@gmail.com>
2025-09-03 16:36:04 -0300

    refactor: implement working logic in Program.cs

--- AbstractProject/Program.cs
+++ AbstractProject/Program.cs
@@ -1,2 +1,18 @@
 ﻿using AbstractProject.Entities;
 
+Console.WriteLine("Enter the number of tax payers: ");
+var numberPayers = int.Parse(Console.ReadLine() ?? "0");
+
+for (int i = 1; i <= numberPayers; i++)
+{
+	Console.WriteLine($"Tax payer #{i} data: ");
+
+	Console.Write("Individual or company (i/c)? ");
+	var typePerson = char.Parse(Console.ReadLine() ?? "i");
+
+	Console.Write("Name: ");
+	var name = Console.ReadLine() ?? "0";
+
+	Console.Write("Annual Income: ");
+	var annualIncome = double.Parse(Console.ReadLine() ?? "0");
+}
